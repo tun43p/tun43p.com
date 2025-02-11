0ba797a9b4b0ea7be53596a667dcaa69aee6b0a2
tun43p <tun43p@icloud.com>
2025-02-11 10:24:18 +0100

    chore: install and setup tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,8 @@
 
 import { render } from "solid-js/web";
 import "./index.css";
-import App from "./app/app";
+
+import App from "./app";
 
 const root = document.getElementById("root");
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [tailwindcss(), solid()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: "esnext",
+  },
 });
