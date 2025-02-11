1a5d2ca8945a6e4ee7bf9b6627dd02feea5ddcff
tun43p <tun43p@icloud.com>
2025-02-11 14:49:07 +0100

    feat: optimize seo

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import ssr from "vite-plugin-ssr/plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), solid()],
+  plugins: [tailwindcss(), solid(), ssr()],
   server: {
     port: 3000,
   },
+  optimizeDeps: {
+    include: ["solid-js"],
+  },
   build: {
     target: "esnext",
+    minify: "esbuild",
+    sourcemap: false,
   },
 });
